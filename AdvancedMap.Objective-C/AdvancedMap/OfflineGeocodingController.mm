2b42ab5ee14c498a3a70733314fc237da4ed3580
Mark Tehver <mark.tehver@gmail.com>
2017-01-05 11:22:18 +0200

    Working geocoding sample, cleanup

---
+++ AdvancedMap.Objective-C/AdvancedMap/OfflineGeocodingController.mm
@@ -0,0 +1,249 @@
+#import "MapBaseController.h"
+#import <UIKit/UIKit.h>
+
+@interface OfflineGeocodingController : MapBaseController <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UIGestureRecognizerDelegate>
+
+@property NTGeocodingService* geocodingService;
+@property NTLocalVectorDataSource* dataSource;
+@property NTVectorElement* oldGeometry;
+@property NTVectorElement* oldClickLabel;
+@property NSMutableArray* addresses;
+@property UITextField* searchField;
+@property UITableView* autocompleteTableView;
+
+@end
+
+@implementation OfflineGeocodingController
+
+-(void)viewDidLoad
+{
+    NTProjection* proj = [[self.mapView getOptions] getBaseProjection];
+    self.geocodingService = [[NTOSMOfflineGeocodingService alloc] initWithProjection:proj path:[NTAssetUtils calculateResourcePath:@"estonia-latest.sqlite"]];
+    
+    self.dataSource = [[NTLocalVectorDataSource alloc] initWithProjection:proj];
+    NTVectorLayer *layer = [[NTVectorLayer alloc] initWithDataSource:self.dataSource];
+    
+    [[self.mapView getLayers] add:layer];
+    
+    NTMapPos* pos = [proj fromWgs84:[[NTMapPos alloc] initWithX:26.7 y:58.38]];
+    [self.mapView setFocusPos:pos durationSeconds:0];
+    [self.mapView setZoom:14.5f durationSeconds:0];
+
+    // Create search UI
+    float width = self.navigationController.navigationBar.frame.size.width;
+    float height = self.navigationController.navigationBar.frame.origin.y + self.navigationController.navigationBar.frame.size.height;
+    
+    self.addresses = [[NSMutableArray alloc] init];
+    
+    UITextField* tf = [[UITextField alloc] initWithFrame:CGRectMake(10, 10, width - 20, 60)];
+    tf.textColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
+    tf.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.5];
+    tf.autocorrectionType = UITextAutocorrectionTypeNo;
+    tf.placeholder = @"Type address...";
+    tf.delegate = self;
+    self.searchField = tf;
+    
+    UITableView* tv = [[UITableView alloc] initWithFrame:CGRectMake(10, 70, width - 20, 240) style:UITableViewStylePlain];
+    tv.delegate = self;
+    tv.dataSource = self;
+    tv.scrollEnabled = YES;
+    tv.hidden = YES;
+    self.autocompleteTableView = tv;
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, height, width, 400)];
+    [view addSubview:tf];
+    [view addSubview:tv];
+    
+    [self.view addSubview:view];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    tap.delegate = self;
+    
+    [self.view addGestureRecognizer:tap];
+    
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self.mapView setMapEventListener:nil];
+}
+
+-(void)geocode:(NSString*)text autocomplete:(BOOL)autocomplete
+{
+    [self hideGeocodingResult];
+    
+    // Calculation should be in background thread
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSTimeInterval start = [NSDate timeIntervalSinceReferenceDate];
+        
+        NTGeocodingRequest* request = [[NTGeocodingRequest alloc] initWithQuery:text];
+        
+        // TODO: set autocomplete mode
+        NTGeocodingResultVector* results = [self.geocodingService calculateAddresses:request];
+        
+        NSTimeInterval duration = [NSDate timeIntervalSinceReferenceDate] - start;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // In autocomplete mode just fill the autocomplete address list and reload tableview
+            // In full geocode mode, show the result
+            if (autocomplete)
+            {
+                [self.addresses removeAllObjects];
+                for (int i = 0; i < [results size]; i++) {
+                    [self.addresses addObject:[results get:i]];
+                }
+                [self.autocompleteTableView reloadData];
+            }
+            else
+            {
+                if ([results size] > 0)
+                {
+                    [self showGeocodingResult:[results get:0]];
+                }
+            }
+            
+            NSLog(@"Geocoding: %d results, took %0.3fs", [results size], duration);
+        });
+    });
+}
+
+-(void)hideGeocodingResult
+{
+    // Remove old click label
+    if (_oldClickLabel)
+    {
+        [self.dataSource remove:_oldClickLabel];
+        _oldClickLabel = nil;
+    }
+    if (_oldGeometry)
+    {
+        [self.dataSource remove:_oldGeometry];
+        _oldGeometry = nil;
+    }
+}
+
+-(void)showGeocodingResult:(NTGeocodingResult*)result
+{
+    // Configure style
+    NTBalloonPopupStyleBuilder* styleBuilder = [[NTBalloonPopupStyleBuilder alloc] init];
+    [styleBuilder setLeftMargins:[[NTBalloonPopupMargins alloc] initWithLeft:0 top:0 right:0 bottom:0]];
+    [styleBuilder setTitleMargins:[[NTBalloonPopupMargins alloc] initWithLeft:6 top:3 right:6 bottom:3]];
+    // Make sure this label is shown on top all other labels
+    [styleBuilder setPlacementPriority:10];
+    
+    NTGeometry* geom = [result getGeometry];
+    if (geom) {
+        NTColor* color = [[NTColor alloc] initWithR:0 g:100 b:200 a:150];
+        
+        // Build styles for the displayed geometry
+        NTPointStyleBuilder* pointStyleBuilder = [[NTPointStyleBuilder alloc] init];
+        [pointStyleBuilder setColor: color];
+        
+        NTLineStyleBuilder* lineStyleBuilder = [[NTLineStyleBuilder alloc] init];
+        [lineStyleBuilder setColor: color];
+        
+        NTPolygonStyleBuilder* polygonStyleBuilder = [[NTPolygonStyleBuilder alloc] init];
+        [polygonStyleBuilder setColor: color];
+        
+        NTVectorElement* elem = nil;
+        if ([geom isKindOfClass:[NTPointGeometry class]]) {
+            elem = [[NTPoint alloc] initWithGeometry:(NTPointGeometry*)geom style:[pointStyleBuilder buildStyle]];
+        }
+        if ([geom isKindOfClass:[NTLineGeometry class]]) {
+            elem = [[NTLine alloc] initWithGeometry:(NTLineGeometry*)geom style:[lineStyleBuilder buildStyle]];
+        }
+        if ([geom isKindOfClass:[NTPolygonGeometry class]]) {
+            elem = [[NTPolygon alloc] initWithGeometry:(NTPolygonGeometry*)geom style:[polygonStyleBuilder buildStyle]];
+        }
+        if ([geom isKindOfClass:[NTMultiGeometry class]]) {
+            NTGeometryCollectionStyleBuilder* geomCollectionStyleBuilder = [[NTGeometryCollectionStyleBuilder alloc] init];
+            [geomCollectionStyleBuilder setPointStyle:[pointStyleBuilder buildStyle]];
+            [geomCollectionStyleBuilder setLineStyle:[lineStyleBuilder buildStyle]];
+            [geomCollectionStyleBuilder setPolygonStyle:[polygonStyleBuilder buildStyle]];
+            elem = [[NTGeometryCollection alloc] initWithGeometry:(NTMultiGeometry*)geom style:[geomCollectionStyleBuilder buildStyle]];
+        }
+        
+        // Show the element and pan/zoom the view to the element
+        if (elem) {
+            [self.dataSource add:elem];
+            _oldGeometry = elem;
+            
+            NTScreenBounds* screenBounds = [[NTScreenBounds alloc] initWithMin:[[NTScreenPos alloc] initWithX:10 y:10] max:[[NTScreenPos alloc] initWithX:self.mapView.drawableWidth - 20 y:self.mapView.drawableHeight - 20]];
+            [self.mapView moveToFitBounds:[geom getBounds] screenBounds:screenBounds integerZoom:NO durationSeconds:0.3f];
+        }
+        
+        // Show popup
+        NTMapPos* pos = [geom getCenterPos];
+        NSString* title = @"";
+        NSString* desc = [result description];
+        NTBalloonPopup* clickPopup = [[NTBalloonPopup alloc] initWithPos:pos style:[styleBuilder buildStyle] title:title desc:desc];
+        [self.dataSource add:clickPopup];
+        _oldClickLabel = clickPopup;
+    }
+}
+
+-(void)dismissKeyboard
+{
+    [self.searchField resignFirstResponder];
+    self.autocompleteTableView.hidden = YES;
+}
+
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gesture shouldReceiveTouch:(UITouch *)touch
+{
+    // Make sure our views get touch events
+    if (touch.view == self.autocompleteTableView || touch.view == self.searchField) {
+        return YES;
+    }
+    return NO;
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.searchField resignFirstResponder];
+    self.autocompleteTableView.hidden = YES;
+
+    [self geocode:self.searchField.text autocomplete:NO];
+    return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    self.autocompleteTableView.hidden = NO;
+    NSString *substring = [NSString stringWithString:textField.text];
+    substring = [substring stringByReplacingCharactersInRange:range withString:string];
+
+    [self geocode:substring autocomplete:YES];
+    return YES;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger) section
+{
+    return self.addresses.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *AutoCompleteRowIdentifier = @"AutoCompleteRowIdentifier";
+    UITableViewCell *cell = nil;
+    cell = [tableView dequeueReusableCellWithIdentifier:AutoCompleteRowIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:AutoCompleteRowIdentifier];
+    }
+    
+    cell.tag = indexPath.row;
+    cell.textLabel.text = [[self.addresses objectAtIndex:indexPath.row] description];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *selectedCell = [tableView cellForRowAtIndexPath:indexPath];
+    [self.searchField resignFirstResponder];
+    self.autocompleteTableView.hidden = YES;
+    
+    [self hideGeocodingResult];
+    [self showGeocodingResult:[self.addresses objectAtIndex:selectedCell.tag]];
+}
+
+@end
--- AdvancedMap.Objective-C/AdvancedMap/OfflineReverseGeocodingController.mm
+++ AdvancedMap.Objective-C/AdvancedMap/OfflineReverseGeocodingController.mm
@@ -3,8 +3,10 @@
 
 @interface OfflineReverseGeocodingController : MapBaseController
 
-@property NTReverseGeocodingService *reverseGeocodingService;
-@property NTLocalVectorDataSource *dataSource;
+@property NTReverseGeocodingService* reverseGeocodingService;
+@property NTLocalVectorDataSource* dataSource;
+@property (strong, nonatomic) NTBalloonPopup* oldClickLabel;
+@property (strong, nonatomic) NTVectorElement* oldGeometry;
 
 @end
 
@@ -14,8 +16,6 @@
 -(void)onMapClicked:(NTMapClickInfo *)mapClickInfo;
 
 @property (strong, nonatomic) OfflineReverseGeocodingController* controller;
-@property (strong, nonatomic) NTBalloonPopup* oldClickLabel;
-@property (strong, nonatomic) NTVectorElement* oldGeometry;
 
 @end
 
@@ -46,6 +46,80 @@
     [self.mapView setMapEventListener:nil];
 }
 
+-(void)hideGeocodingResult
+{
+    // Remove old click label
+    if (_oldClickLabel)
+    {
+        [self.dataSource remove:_oldClickLabel];
+        _oldClickLabel = nil;
+    }
+    if (_oldGeometry)
+    {
+        [self.dataSource remove:_oldGeometry];
+        _oldGeometry = nil;
+    }
+}
+
+-(void)showGeocodingResult:(NTGeocodingResult*)result position:(NTMapPos*)pos
+{
+    // Configure style
+    NTBalloonPopupStyleBuilder* styleBuilder = [[NTBalloonPopupStyleBuilder alloc] init];
+    [styleBuilder setLeftMargins:[[NTBalloonPopupMargins alloc] initWithLeft:0 top:0 right:0 bottom:0]];
+    [styleBuilder setTitleMargins:[[NTBalloonPopupMargins alloc] initWithLeft:6 top:3 right:6 bottom:3]];
+    // Make sure this label is shown on top all other labels
+    [styleBuilder setPlacementPriority:10];
+    
+    NTGeometry* geom = result ? [result getGeometry] : nil;
+    if (geom) {
+        NTColor* color = [[NTColor alloc] initWithR:0 g:100 b:200 a:150];
+        
+        // Build styles for the displayed geometry
+        NTPointStyleBuilder* pointStyleBuilder = [[NTPointStyleBuilder alloc] init];
+        [pointStyleBuilder setColor: color];
+        
+        NTLineStyleBuilder* lineStyleBuilder = [[NTLineStyleBuilder alloc] init];
+        [lineStyleBuilder setColor: color];
+        
+        NTPolygonStyleBuilder* polygonStyleBuilder = [[NTPolygonStyleBuilder alloc] init];
+        [polygonStyleBuilder setColor: color];
+        
+        NTVectorElement* elem = nil;
+        if ([geom isKindOfClass:[NTPointGeometry class]]) {
+            elem = [[NTPoint alloc] initWithGeometry:(NTPointGeometry*)geom style:[pointStyleBuilder buildStyle]];
+        }
+        if ([geom isKindOfClass:[NTLineGeometry class]]) {
+            elem = [[NTLine alloc] initWithGeometry:(NTLineGeometry*)geom style:[lineStyleBuilder buildStyle]];
+        }
+        if ([geom isKindOfClass:[NTPolygonGeometry class]]) {
+            elem = [[NTPolygon alloc] initWithGeometry:(NTPolygonGeometry*)geom style:[polygonStyleBuilder buildStyle]];
+        }
+        if ([geom isKindOfClass:[NTMultiGeometry class]]) {
+            NTGeometryCollectionStyleBuilder* geomCollectionStyleBuilder = [[NTGeometryCollectionStyleBuilder alloc] init];
+            [geomCollectionStyleBuilder setPointStyle:[pointStyleBuilder buildStyle]];
+            [geomCollectionStyleBuilder setLineStyle:[lineStyleBuilder buildStyle]];
+            [geomCollectionStyleBuilder setPolygonStyle:[polygonStyleBuilder buildStyle]];
+            elem = [[NTGeometryCollection alloc] initWithGeometry:(NTMultiGeometry*)geom style:[geomCollectionStyleBuilder buildStyle]];
+        }
+        
+        // Show the element and pan/zoom the view to the element
+        if (elem) {
+            [self.dataSource add:elem];
+            _oldGeometry = elem;
+            
+            NTScreenBounds* screenBounds = [[NTScreenBounds alloc] initWithMin:[[NTScreenPos alloc] initWithX:10 y:10] max:[[NTScreenPos alloc] initWithX:self.mapView.drawableWidth - 20 y:self.mapView.drawableHeight - 20]];
+            [self.mapView moveToFitBounds:[geom getBounds] screenBounds:screenBounds integerZoom:NO durationSeconds:0.3f];
+        }
+    }
+
+    // Show popup
+    NSString* title = @"";
+    NSString* desc = result ? [result description] : @"No address found";
+    NTBalloonPopup* clickPopup = [[NTBalloonPopup alloc] initWithPos:pos style:[styleBuilder buildStyle] title:title desc:desc];
+    [self.dataSource add:clickPopup];
+    _oldClickLabel = clickPopup;
+}
+
 @end
 
 @implementation ReverseGeocodingMapEventListener
@@ -57,95 +131,23 @@
 
 -(void)onMapClicked:(NTMapClickInfo*)clickInfo
 {
-    // Remove old click label
-    if (_oldClickLabel)
-    {
-        [self.controller.dataSource remove:_oldClickLabel];
-        _oldClickLabel = nil;
-    }
-    if (_oldGeometry)
-    {
-        [self.controller.dataSource remove:_oldGeometry];
-        _oldGeometry = nil;
-    }
-    
-    NTReverseGeocodingRequest* request = [[NTReverseGeocodingRequest alloc] initWithProjection:[self.controller.dataSource getProjection] point:[clickInfo getClickPos]];
-
-    NSTimeInterval start = [NSDate timeIntervalSinceReferenceDate];
+    [self.controller hideGeocodingResult];
     
     // Calculation should be in background thread
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSTimeInterval start = [NSDate timeIntervalSinceReferenceDate];
+        
+        NTReverseGeocodingRequest* request = [[NTReverseGeocodingRequest alloc] initWithProjection:[self.controller.dataSource getProjection] point:[clickInfo getClickPos]];
         
         NTGeocodingResultVector* results = [self.controller.reverseGeocodingService calculateAddresses:request];
         
-        dispatch_async(dispatch_get_main_queue(), ^{
-    
-            NSTimeInterval duration = [NSDate timeIntervalSinceReferenceDate] - start;
-
-            NTBalloonPopupStyleBuilder* styleBuilder = [[NTBalloonPopupStyleBuilder alloc] init];
-            // Configure style
-            [styleBuilder setLeftMargins:[[NTBalloonPopupMargins alloc] initWithLeft:0 top:0 right:0 bottom:0]];
-            [styleBuilder setTitleMargins:[[NTBalloonPopupMargins alloc] initWithLeft:6 top:3 right:6 bottom:3]];
-            // Make sure this label is shown on top all other labels
-            [styleBuilder setPlacementPriority:10];
-            
-            NTMapPos* pos = [clickInfo getClickPos];
-            NSString* title = @"No close address found";
-            NSString* desc = @"";
-            if ([results size] > 0) {
-                title = @"Found address";
-                desc = [[results get:0] description];
-                
-                NTGeometry* geom = [[results get:0] getGeometry];
-                if (geom) {
-                    pos = [geom getCenterPos];
-
-                    NTColor* color = [[NTColor alloc] initWithR:0 g:100 b:200 a:150];
-                    
-                    NTPointStyleBuilder* pointStyleBuilder = [[NTPointStyleBuilder alloc] init];
-                    [pointStyleBuilder setColor: color];
-                    
-                    NTLineStyleBuilder* lineStyleBuilder = [[NTLineStyleBuilder alloc] init];
-                    [lineStyleBuilder setColor: color];
-                    
-                    NTPolygonStyleBuilder* polygonStyleBuilder = [[NTPolygonStyleBuilder alloc] init];
-                    [polygonStyleBuilder setColor: color];
-
-                    NTVectorElement* elem = nil;
-                    if ([geom isKindOfClass:[NTPointGeometry class]]) {
-                        elem = [[NTPoint alloc] initWithGeometry:(NTPointGeometry*)geom style:[pointStyleBuilder buildStyle]];
-                    }
-                    if ([geom isKindOfClass:[NTLineGeometry class]]) {
-                        elem = [[NTLine alloc] initWithGeometry:(NTLineGeometry*)geom style:[lineStyleBuilder buildStyle]];
-                    }
-                    if ([geom isKindOfClass:[NTPolygonGeometry class]]) {
-                        elem = [[NTPolygon alloc] initWithGeometry:(NTPolygonGeometry*)geom style:[polygonStyleBuilder buildStyle]];
-                    }
-                    if ([geom isKindOfClass:[NTMultiGeometry class]]) {
-                        NTGeometryCollectionStyleBuilder* geomCollectionStyleBuilder = [[NTGeometryCollectionStyleBuilder alloc] init];
-                        [geomCollectionStyleBuilder setPointStyle:[pointStyleBuilder buildStyle]];
-                        [geomCollectionStyleBuilder setLineStyle:[lineStyleBuilder buildStyle]];
-                        [geomCollectionStyleBuilder setPolygonStyle:[polygonStyleBuilder buildStyle]];
-                        elem = [[NTGeometryCollection alloc] initWithGeometry:(NTMultiGeometry*)geom style:[geomCollectionStyleBuilder buildStyle]];
-                    }
-
-                    if (elem) {
-                        [self.controller.dataSource add:elem];
-                        _oldGeometry = elem;
-                    }
-                }
-            }
-            
-            // for lines and polygons set label to click location
-            NTBalloonPopup* clickPopup = [[NTBalloonPopup alloc] initWithPos:pos
-                                                       style:[styleBuilder buildStyle]
-                                                       title:title
-                                                        desc:desc];
-            [self.controller.dataSource add:clickPopup];
-            _oldClickLabel = clickPopup;
-            
-            NSLog(@"Reverse geocoding: %@, took %0.3fs", desc, duration);
-        });
+        NTGeocodingResult* result = [results size] > 0 ? [results get:0] : nil;
+        
+        NSTimeInterval duration = [NSDate timeIntervalSinceReferenceDate] - start;
+        
+        [self.controller showGeocodingResult:result position:[clickInfo getClickPos]];
+        
+        NSLog(@"Reverse geocoding: %d results, took %0.3fs", [results size], duration);
     });
 }
 
